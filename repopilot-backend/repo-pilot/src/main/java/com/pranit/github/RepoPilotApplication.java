c05123ada9649325d853f79e34f2d980219df0b6
Pranit Bhangale <pranitbhangale1@gmail.com>
2026-09-14 11:18:48 +0530

    Added: Initial Project

---
+++ repopilot-backend/repo-pilot/src/main/java/com/pranit/github/RepoPilotApplication.java
@@ -0,0 +1,13 @@
+package com.pranit.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoPilotApplication.class, args);
+	}
+
+}
